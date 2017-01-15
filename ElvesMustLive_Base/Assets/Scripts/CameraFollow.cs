893aa35e9d001d06451fd30b6a0393b32ff89e4c
Nat <louis.holleville@epita.fr>
2017-01-15 21:58:31 +0100

    Ajouts de scripts :  - suivi du joueur par la camera  - mouvement WASD du joueur

---
+++ ElvesMustLive_Base/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform player;
+    Vector3 offset; //distance joueur/camera
+
+    void Start()
+    {
+        offset = transform.position - player.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 CameraNewPos = player.position + offset;
+        transform.position = Vector3.Lerp(transform.position, CameraNewPos, 10f * Time.deltaTime); 
+        // creer le mouvement avec un leger effet "smooth" a la camera
+    }
+}
