314c21f641c02f15d917797fa75f656a78e5dea2
Thetoto <thomas.lupin@gmail.com>
2017-01-16 20:03:29 +0100

    Ajout scene avec script vie/mort/instantiate + prefab player&cam

---
+++ ElvesMustLive_Base/Assets/Scripts/Game.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    Joueur player1;
+
+    // Use this for initialization
+        void Start () {
+            player1 = new Joueur();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (player1.Health == 0)
+        {
+            player1.death();
+            player1 = new Joueur();
+        }
+        if (Input.GetKey("h"))
+        {
+            player1.Health -= 10;
+            Debug.Log(player1.Health);
+        }
+	}
+}
