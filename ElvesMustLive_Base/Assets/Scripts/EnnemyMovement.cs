5769918c305a8cd12724dac16b8b9bb8dbf2defd
Nat <louis.holleville@epita.fr>
2017-01-16 19:08:22 +0100

    Tentative de nav 0.1

---
+++ ElvesMustLive_Base/Assets/Scripts/EnnemyMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class EnnemyMovement : MonoBehaviour
+{
+	Transform player;               // Reference to the player's position.
+	//PlayerHealth playerHealth;       Reference to the player's health.
+	//EnemyHealth enemyHealth;         Reference to this enemy's health.
+	NavMeshAgent nav;               // Reference to the nav mesh agent.
+	SphereCollider coll;
+
+	void Awake ()
+	{
+		// Set up the references.
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		coll = GetComponent<SphereCollider> ();
+		//playerHealth = player.GetComponent <PlayerHealth> ();
+		//enemyHealth = GetComponent <EnemyHealth> ();
+		nav = GetComponent <NavMeshAgent> ();
+	}
+	void Start ()
+	{
+	}
+
+
+	void Update ()
+	{
+		//if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+		if (OnTriggerEnter(coll)==true)
+		{
+			Debug.Log ("destination set");
+			nav.SetDestination (player.position);
+		}
+
+		if (OnTriggerExit(coll)==true)
+		{
+		Debug.Log ("destination unset");
+		nav.enabled = false;
+		}
+	} 
+	bool OnTriggerEnter(Collider coll)
+	{
+		if (coll.tag == "Player")
+		{
+			Debug.Log (" enter");
+			return true;
+		}
+		return false;
+	}
+
+	bool OnTriggerExit(Collider coll)
+	{
+		if (coll.tag =="Player")
+		{
+			return true;
+		}
+		return false;
+	}
+}
