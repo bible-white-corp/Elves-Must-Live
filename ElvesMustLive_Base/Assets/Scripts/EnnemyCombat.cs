2ce30602a568dd9de160c05107c949d7e78deb63
Nat <louis.holleville@epita.fr>
2017-01-18 23:14:52 +0100

    Debut de coding du script de combat

---
+++ ElvesMustLive_Base/Assets/Scripts/EnnemyCombat.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyCombat : MonoBehaviour 
+{
+	//Faut qu'on discute de ce qu'on va faire...
+
+	/*using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.AI;
+
+	public class EnnemyCombat : MonoBehaviour 
+	{
+
+		int health;
+		Animator anim;
+		public float sinkspeed = 2.5f;
+		CapsuleCollider CapsColl;
+		bool IsSinking;
+		bool IsDead;
+		NavMeshAgent nav;
+
+		void Start () 
+		{
+			health = 30;
+			anim = GetComponent<Animator> ();
+			CapsColl = GetComponent<CapsuleCollider> ();
+			nav = GetComponent<NavMeshAgent> ();
+		}
+
+		void Update()
+		{
+			if(IsSinking)
+			{
+				// c'est le machin qui fait fondre l'ennemi
+				transform.Translate (-Vector3.up * sinkspeed * Time.deltaTime);
+				if (Input.GetKey("k"))
+				{
+					TakeDamage (30);
+				}
+			}
+		}
+
+		public void TakeDamage(int amount)
+		{
+			if (IsDead)
+			{
+				return;
+			}
+			health -= amount;
+
+			if(health <= 0)
+			{
+				Death ();
+			}
+		}
+		void Death ()
+		{
+			IsDead = true;
+			CapsColl.isTrigger = true;
+			anim.SetBool ("Dead", true);
+			StartSinking ();
+		}
+		public void StartSinking ()
+		{
+			nav.enabled = false;
+			GetComponent <Rigidbody> ().isKinematic = true;
+			IsSinking = true;
+			Destroy (gameObject, 2f);
+		}
+	}
+*/
+}
