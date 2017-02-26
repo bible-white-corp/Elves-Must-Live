b0075ea068aaf5a47eef48f176688633dc511027
Thetoto <thomas.lupin@gmail.com>
2017-02-26 11:54:37 +0100

    Very very small things...

--- ElvesMustLive_Base/Assets/Scripts/Turrets/RayCast.cs
+++ ElvesMustLive_Base/Assets/Scripts/Turrets/RayCast.cs
@@ -54,6 +54,7 @@ public class RayCast : MonoBehaviour {
 			{
 				tempProspect= Instantiate (prospect,transform.position, new Quaternion(0,0,0,0),transform);
 				//script2 = tempProspect.GetComponent<Prerenducollision> ();
+				tempProspect.transform.localRotation = Quaternion.Euler (new Vector3 (0, 180, 0));
 				i = tempProspect.transform.localRotation.y;
 
 				//materials = prerendu.GetComponentsInChildren<Material>();
