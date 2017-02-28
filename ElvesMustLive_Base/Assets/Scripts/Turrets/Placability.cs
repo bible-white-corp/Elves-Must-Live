b51c3c405a8a2b84cad975da89c79fb9518e3d63
Nat <louis.holleville@epita.fr>
2017-02-28 10:39:02 +0100

    Amelioration du systeme de preview, mais tjr buggé

---
+++ ElvesMustLive_Base/Assets/Scripts/Turrets/Placability.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placability : MonoBehaviour {
+
+	Prerenducollision script;
+	bool IsPlacable;
+
+	void Start () 
+	{
+		script = GetComponentInParent<Prerenducollision> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		IsPlacable = true;
+	}
+	void LateUpdate () 
+	{
+		script.IsPlacable (IsPlacable);
+	}
+	void OnTriggerStay(Collider coll)
+	{
+		this.IsPlacable =false;
+	}
+
+}
