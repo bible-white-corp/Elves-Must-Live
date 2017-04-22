cd2d7dbd23f19790900d463f4c34324c1b583891
Nat <louis.holleville@epita.fr>
2017-04-22 17:26:59 +0200

    [NEW TURRETS] Projector: light ennemies and slow them by blinding

---
+++ ElvesMustLive_Base/Assets/Scripts/Turrets/Projector/Projector_buff.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Projector_buff : MonoBehaviour {
+
+	public float SpeedReduction;
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Shootable") 
+		{
+			other.GetComponent<NavMeshAgent> ().speed -= SpeedReduction;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Shootable") 
+		{
+			other.GetComponent<NavMeshAgent> ().speed += SpeedReduction;
+		}
+	}
+	
+}
