98e4e0d272a4f5509b8fd106cda633ec68f0aa7e
Thetoto <thomas.lupin@gmail.com>
2017-04-23 19:38:14 +0200

    Pause button (Work approximatly)

---
+++ ElvesMustLive_Base/Assets/UI/Pause.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public bool paused = false;
+
+    public void PauseGame()
+    {
+        if (paused)
+        {
+            Time.timeScale = 1;
+            paused = false;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            paused = true;
+        }
+    }
+}
