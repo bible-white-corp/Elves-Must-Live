720207e522ad1136e86310c795e1a81f5c8217de
Thetoto <thomas.lupin@gmail.com>
2017-04-25 15:13:05 +0200

    TP opérationnel Correction d'un bug sur le script du perso Map 2 opérationnelle

---
+++ ElvesMustLive_Base/Assets/Map/MAP 2 multipath/TPScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPScript : MonoBehaviour {
+
+    public GameObject TpPosition;
+    public bool JustTP;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (TpPosition.GetComponent<TPScript>().JustTP)
+            {
+                return;
+            }
+            JustTP = true;
+            other.transform.position = TpPosition.transform.position;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        TpPosition.GetComponent<TPScript>().JustTP = false;
+    }
+}
