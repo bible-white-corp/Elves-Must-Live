1473fb845b660efa2953684f0912f2bd6b8a8fe1
Thetoto <thomas.lupin@gmail.com>
2017-05-12 16:44:07 +0200

    Ajout d'une magnifique Intro + J'ai essayer de combler les vides dans la Map 1

---
+++ ElvesMustLive_Base/Assets/UI/PlayVideo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayVideo : MonoBehaviour {
+
+
+	public MovieTexture movie;
+	public AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<RawImage>().texture = movie as MovieTexture;
+
+		movie.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)){
+			SceneManager.LoadScene("Lobby");
+		}
+	}
+}
