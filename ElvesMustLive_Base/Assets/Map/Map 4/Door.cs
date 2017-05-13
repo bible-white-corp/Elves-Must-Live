b9e0aa15ebf42225f0707ae074d9fac7c22a0aaa
Thetoto <thomas.lupin@gmail.com>
2017-05-14 00:36:28 +0200

    Finish Map 4 !

---
+++ ElvesMustLive_Base/Assets/Map/Map 4/Door.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public int maxLife = 500;
+    public GameObject door;
+    int woodLife;
+
+	// Use this for initialization
+	void Start () {
+        woodLife = maxLife;	
+	}
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Shootable")
+        {
+            woodLife -= 1;
+        }
+        else if (!door.GetActive() && other.tag == "Player")
+        {
+            door.SetActive(true);
+            woodLife = maxLife;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+        if (woodLife < 0)
+        {
+            door.SetActive(false);
+        }
+	}
+}
