1aec1d283ee31e586a506ce04500a757266e8462
Thetoto <thomas.lupin@gmail.com>
2017-05-19 13:38:27 +0200

    Tuto a jour.

--- ElvesMustLive_Base/Assets/Tutorial/Tuto.cs
+++ ElvesMustLive_Base/Assets/Tutorial/Tuto.cs
@@ -31,109 +31,99 @@ public class Tuto : GameMode {
         switch (i)
         {
             case 0:
+                label.text = Localization.Get("tuto_" + i);
                 i++;
-                label.text = "Welcome to the[99ff00]Elves Must Live tutorial[-] !" +
-                    "\nYou can move with[99ff00]ZQSD[-]" +
-                    "\nPress[99ff00]Enter[-] to continue";//tuto_1
                 break;
             case 1:
                 if (Input.GetKeyDown(KeyCode.Return))
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
-                    label.text = "Good ! I will summon a orc. Be ready to attack him with [99ff00]Left Clic[-] " +
-                        "\nPress [99ff00]Enter[-] when you are ready !";//tuto_2
                 }
                 break;
             case 2:
                 if (Input.GetKeyDown(KeyCode.Return))
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
                     PhotonNetwork.InstantiateSceneObject("Ennemy0", gameObject.transform.position, Quaternion.identity, 0, new object[] { });
-                    label.text = "Kill the orc with [99ff00]Left Clic[-] !!"; //tuto_3
                 }
                 break;
             case 3:
                 if (!game.ennemyInMap)
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
-                    label.text = "Good ! You kill a orc and earn 10 gold !" +
-                        "\nPress [99ff00]Enter[-] to continue.";//tuto_4
                 }
                 break;
             case 4:
-                
+
                 if (Input.GetKeyDown(KeyCode.Return))
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
-                    label.text = "You have a 10 of money. You can only buy a [0000ff]Cannon[-]" +
-                        "\nPress [99ff00]B[-] to start building a turret.";//tuto_4
                 }
                 break;
             case 5:
-                
+
                 if (Input.GetKeyDown(KeyCode.B))
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
-                    label.text = "Choose an appropriate place to build your [0000ff]Cannon[-] (When the cannon is [99ff00]green[-])" +
-                        "\nPress [99ff00]B[-] to place the turret.";//tuto_5
                 }
                 break;
             case 6:
                 if (Input.GetKeyDown(KeyCode.B))
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
                     PhotonNetwork.InstantiateSceneObject("Ennemy0", gameObject.transform.position, Quaternion.identity, 0, new object[] { });
                     PhotonNetwork.InstantiateSceneObject("Ennemy0", gameObject.transform.position, Quaternion.identity, 0, new object[] { });
                     PhotonNetwork.InstantiateSceneObject("Ennemy0", gameObject.transform.position, Quaternion.identity, 0, new object[] { });
-                    label.text = "Oups ! 3 orcs are comming ! Dont worry, your cannon will help you !" +
-                        "\nAttack the orcs and watch your cannon.";//tuto_6
+
                 }
                 break;
             case 7:
                 if (GameObject.FindGameObjectWithTag("Shootable") == null)
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
-                    label.text = "Good ! You have a lot of money now ! You can buy (or upgrade) other turrets !" +
-                        "\nPress [99ff00]V[-] to open the [ff0000]Shop[-].";//tuto7
                 }
                 break;
             case 8:
                 if (Input.GetKeyDown(KeyCode.V))
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
-                    label.text = "Fine. Here you can click on a turret, and select Buy to optain it. Be carrefull, you must have sufficient funds." +
-                        "\nChoose a turret and buy it !";//tuto_8
                 }
                 break;
             case 9:
                 if (game.masterClient.raycast.AvailableTurrets.Count >= 2)
                 {
-                    i++;
                     var turret = game.masterClient.raycast.AvailableTurrets.Find(x => !x.Key.Contains("Cannon"));
-                    label.text = "[0000ff]" + Localization.Get(turret.Key.Split('_')[0]) + "[-] ? Perfect ! Leave the shop with [99ff00]Escape[-] and press B to enter in build mode.";
-				//la on a un pbl...#Nat
+                    label.text = "[0000ff]" + Localization.Get(turret.Key.Split('_')[0]) + "[-] ? " + Localization.Get("tuto_" + i);
+                    i++;
                 }
                 break;
             case 10:
                 if (Input.GetKeyDown(KeyCode.B))
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
-                    label.text = "As you can see, you have now an another turret ! But you don't have enought money... I will give you money."
-                        + "\nPress [99ff00]Enter[-]";//tuto_10
                 }
                 break;
             case 11:
                 if (Input.GetKeyDown(KeyCode.Return))
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
                     game.masterClient.gold = 200;
-                    label.text = "Crap! 200 golds... It's an error... Alright, at least, you can now place your turret."
-					+ "";//tuto_11
                 }
                 break;
             case 12:
                 if (game.masterClient.gold < 200)
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
                     PhotonNetwork.InstantiateSceneObject("Ennemy0", gameObject.transform.position, Quaternion.identity, 0, new object[] { });
                     PhotonNetwork.InstantiateSceneObject("Ennemy0", gameObject.transform.position, Quaternion.identity, 0, new object[] { });
@@ -141,56 +131,51 @@ public class Tuto : GameMode {
                     PhotonNetwork.InstantiateSceneObject("Ennemy0", gameObject.transform.position, Quaternion.identity, 0, new object[] { });
                     PhotonNetwork.InstantiateSceneObject("Ennemy0", gameObject.transform.position, Quaternion.identity, 0, new object[] { });
                     PhotonNetwork.InstantiateSceneObject("Ennemy0", gameObject.transform.position, Quaternion.identity, 0, new object[] { });
-                    label.text = "Shit ! I can't explain without being disturbed ! Kill them !";//tuto_12
+
                 }
                 break;
             case 13:
                 if (!game.ennemyInMap)
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
-                    label.text = "Phew ! Now upgrade your turrets ! Press [99ff00]V[-] and then click on [ff0000]Upgrade[-].";
-                }//tuto_13
+                }
                 break;
             case 14:
                 if (game.masterClient.MyUI.upgrade.gameObject.GetActive())
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
-                    label.text = "Perfect, now click a turret you have, and upgrade it !";//tuto_14
                 }
                 break;
             case 15:
                 if (game.masterClient.raycast.AvailableTurrets.Exists(x => x.Key.Contains("1")))
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
-                    label.text = "Now, click on [ff0000]Weapons[-] on the [ff0000]Shop[-] window.";
-				//tuto_15
                 }
                 break;
             case 16:
                 if (game.masterClient.MyUI.boutikScript.WeaponsSel)
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
-                    label.text = "Here, you can buy weapons. In the game, switch with the mouse scroll wheel." +
-                        "\nSame as turrets, you can upgrade weapons on [ff0000]Upgrades[-] window." +
-                        "\nPress [99ff00]Enter[-] to continue.";//tuto_16
                 }
                 break;
             case 17:
                 if (Input.GetKeyDown(KeyCode.Return))
                 {
+                    label.text = Localization.Get("tuto_" + i);
                     i++;
-				label.text = "Remember that in [99ff00] Elves Must Live [-], victory comes " +
-					"from symbiosis with your turrets. Be as effective as they are on the battlefield and " +
-					"the growing orcs waves will never resist you!"; //tuto_17 #Nat
                 }
                 break;
-		case 18:
-			if (Input.GetKeyDown (KeyCode.Return)) {
-				i++;
-				label.text = "It's the end of this tutorial. Now, go out !" +
-				"\nPress [99ff00]Escape[-] to open the Pause window and select Leave !";
-			}
-			break;
+            case 18:
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    label.text = Localization.Get("tuto_" + i);
+                    i++;
+                }
+                break;
             default:
                 break;
         }
