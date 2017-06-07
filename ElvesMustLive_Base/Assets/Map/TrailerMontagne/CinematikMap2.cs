a8107a458ce8b8cd506e329d17a81bed1ae3b168
Thetoto <thomas.lupin@gmail.com>
2017-06-07 20:03:54 +0200

    [DO NOT PULL - Cinématique] Trailer Map 2

---
+++ ElvesMustLive_Base/Assets/Map/TrailerMontagne/CinematikMap2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematikMap2 : MonoBehaviour {
+
+    public GameObject boss;
+
+	// Use this for initialization
+	void Start () {
+        foreach (var item in GameObject.FindGameObjectsWithTag("Shootable"))
+        {
+            item.transform.LookAt(boss.transform);
+            Random.Range(0, 2);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ElvesMustLive_Base/Assets/Map/TrailerMontagne/Ennemyanim.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemyanim : MonoBehaviour {
+
+    Animator anim;
+    float time = 0;
+    float max;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+        max = Random.Range(1.5f, 3.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if (time > max)
+        {
+            max = Random.Range(1.5f, 3f);
+            time = 0;
+            int rand = Random.Range(0, 10);
+            switch (rand)
+            {
+                case 0:
+                case 1:
+                    break;
+                case 2:
+                case 3:
+                    anim.SetTrigger("idle2");
+                    break;
+                case 4:
+                    anim.SetTrigger("idle1");
+                    break;
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    anim.SetTrigger("spe");
+                    break;
+                default:
+                    break;
+            }
+        }
+	}
+}
