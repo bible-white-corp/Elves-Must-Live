54b63deef03d893666a99b5506b6aa0e4e70f599
Thetoto <thomas.lupin@gmail.com>
2017-06-12 00:10:12 +0200

    [do not pull] Add somes things

---
+++ ElvesMustLive_Base/Assets/Map/Map 4/SkipLvl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipLvl : MonoBehaviour {
+
+    Game game;
+
+	// Use this for initialization
+	void Start () {
+        game = GetComponent<Game>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P) && game.cheats && PlayerPrefs.GetString("Mode") == "History")
+        {
+            GetComponent<WaveGenerator>().winner = true;
+            GetComponent<WaveGenerator>().endGame = true;
+
+        }
+    }
+}
