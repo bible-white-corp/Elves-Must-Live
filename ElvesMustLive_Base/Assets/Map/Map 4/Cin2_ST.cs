ddf0a766ce8c8d96e327348f850ceb4427c59548
Nat PC2 <louis.holleville@epita.fr>
2017-06-11 14:55:19 +0200

    [TRAD] cnématiques ST

---
+++ ElvesMustLive_Base/Assets/Map/Map 4/Cin2_ST.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cin2_ST : MonoBehaviour {
+
+	float timer;
+	public UILabel label;
+	int i;
+	bool un = true;
+	bool deux=true;
+	bool trois=true;
+	bool quatre=true;
+
+	void Start () 
+	{
+		timer = 0;
+		i = 1;
+	}
+		
+	void Update () 
+	{
+		timer += Time.deltaTime;
+		if (un && timer > 21 && timer < 24)
+		{
+			label.text = Localization.Get ("Cin2_"+i);
+			un = false;
+			i += 1;
+		}
+		if (deux && timer > 25 && timer < 29)
+		{
+			label.text = Localization.Get ("Cin2_" + i);
+			deux = false;
+			i += 1;
+		}
+		if (trois && timer > 30&& timer < 32)
+		{
+			label.text = Localization.Get ("Cin2_" + i);
+			trois = false;
+			i += 1;
+		}
+		if (quatre && timer > 34 && timer < 35)
+		{
+			label.text = Localization.Get ("Cin2_" + i);
+			quatre = false;
+			i += 1;
+		}
+	}
+}
